8458820f228cdaf43a8fbf05db017fcc9d1328fb
Tonny <musembitonnyj@gmail.com>
2022-03-25 16:35:43 +0300

    add

---
+++ app/Http/Controllers/MpesaController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use Carbon\Carbon;
+
+use Illuminate\Http\Request;
+use Nette\Utils\Callback;
+
+class MpesaController extends Controller
+{
+
+
+    // stk_push
+
+    public function customerMpesaSTKPush()
+    {
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generateAccessToken()));
+        $curl_post_data = [
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => 174379,
+            'Password' => $this->lipaNaMpesaPassword(),
+            'Timestamp' => Carbon::rawParse('now')->format('YmdHms'),
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => 5,
+            'PartyA' => 254706047229, // replace this with your phone number
+            'PartyB' => 174379,
+            'PhoneNumber' => 254706047229, // replace this with your phone number
+            'CallbackURL'=>  '',
+            'AccountReference' => '',
+            'TransactionDesc' => "Testing stk push on sandbox"
+        ];
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        return $curl_response;
+    }
+
+// Acess_Token Api
+
+    public function generateAccessToken()
+    {
+        $consumer_key="idw3OZml8J94oKBtHFRUvBR8h4yGWuPD";
+        $consumer_secret="MEGe9UQqyEvxfLEX";
+        $credentials = base64_encode($consumer_key.":".$consumer_secret);
+        $url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Basic ".$credentials));
+        curl_setopt($curl, CURLOPT_HEADER,false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $curl_response = curl_exec($curl);
+        $access_token=json_decode($curl_response);
+        return $access_token->access_token;
+    }
+}
